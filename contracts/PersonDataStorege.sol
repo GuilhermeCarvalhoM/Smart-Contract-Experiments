54c8e9f56147022ac1e73dc3ad0ccc7adf65a9b3
GUILHERME\guilh <guilherme.carvalhoacc@gmail.com>
2024-11-28 18:40:16 -0400

    Initial commit: setting up Solidity project

---
+++ contracts/PersonDataStorege.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract PersonDataStorege{
+    string public name = "Jeremias";
+    int private age= 10;
+    bool internal brazilian= true;
+}
+
+
